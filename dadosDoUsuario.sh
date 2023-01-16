05a2e4e34a786c909d54f578c3560b3ce9c1914a
Aurino Manoel <89679772+AurinoMSMF@users.noreply.github.com>
2023-01-16 11:52:26 -0300

    Update dadosDoUsuario.sh

--- dadosDoUsuario.sh
+++ dadosDoUsuario.sh
@@ -6,6 +6,10 @@ echo -e "Nome de usuário: $USER"
 echo -e "Sua home pessoal é: $HOME"
 echo -e "O Shell utilizado é: $SHELL"
 read -p "Digite seu nome completo: " nome
+test -z "$nome"                             \
+&& { sleep 1 ; echo -e "Ops... É necessário seu nome para prosseguir." ; \
+exit ;}                                                                  \
+||
 read -p "Digite sua idade: " idade
 read -p "Digite seu endereço: " ender
 read -p "Digite seu telefone: " tele
@@ -17,4 +21,14 @@ echo -e "Seu endereço é: $ender"
 echo -e "Seu telefone: $tele"
 echo -e "Sua escolaridade: $escolaridade"
 echo -e "Sua profissão: $profiss"
-echo -e "Seus dados estão corretos?[S/N]"
+read -p "Seus dados estão corretos? [S/N] " confirmacao
+confirmacao=${confirmacao^^}
+test $confirmacao = "S"                           \
+&& { sleep 1 ; echo -e "Ok. Guardando dados do usuário." ; \
+sleep 1 ; echo -e "Ok. Guardando dados do usuário.." ;     \
+sleep 1 ; echo -e "Ok. Guardando dados do usuário..." ;    \
+exit ;} \
+|| { sleep 1 ; echo -e "Descartando dados do usuário." ;   \
+sleep 1 ; echo -e "Descartando dados do usuário.." ;       \
+sleep 1 ; echo -e "Descartando dados do usuário..." ;      \
+exit ;}
