e62afb7e6705d190ca46accbb59a5e9f991ce7a7
Aurino <aurinodoc@gmail.com>
2023-01-17 21:46:43 -0300

    Melhorias no script de atualização

--- atualiza_E_Sai.sh
+++ atualiza_E_Sai.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 echo -e "====================SCRIPT DE ATUALIZAÇÃO===================="
-sudo apt update
-sudo apt upgrade
-clear
-echo -e "====================ATUALIZAÇÃO FINALIZADA===================="
+[[ $UID -eq 0 ]] && { echo "Usuário logado como root. Iniciando processo de atualização." ;                               \
+sleep 1 ; apt update ; apt upgrade ; [[ $(type -P neofetch) ]] && neofetch || sleep 1 ;                                      \
+echo -e "====================ATUALIZAÇÃO FINALIZADA====================" ;                                                \
+exit 0 ;}                                                                                                                 \
+ || { echo -e "Ops. O script necessita de privilégios de superusuário para ser executado. Favor, entrar como root." ;     \
+ exit 1 ;}
