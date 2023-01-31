32a1c7dba1e1f703ec253be4cafa9081cc1c49d4
Aurino <aurinodoc@gmail.com>
2023-01-31 19:26:06 -0300

    Melhoria

--- atualiza_E_Sai.sh
+++ atualiza_E_Sai.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo -e "====================SCRIPT DE ATUALIZAÇÃO===================="
 [[ $UID -eq 0 ]] && { echo "Usuário logado como root. Iniciando processo de atualização." ;                               \
-sleep 1 ; apt update ; apt upgrade ; apt autoremove ; clear ;[[ $(type -P neofetch) ]] && neofetch || sleep 1 ;           \
+sleep 1 ; apt update ; apt upgrade ; apt autoclean ; apt autoremove ; clear ;[[ $(type -P neofetch) ]] && neofetch || sleep 1 ;           \
 echo -e "====================ATUALIZAÇÃO FINALIZADA====================" ;                                                \
 exit 0 ;}                                                                                                                 \
  || { echo -e "Ops. O script necessita de privilégios de superusuário para ser executado. Favor, entrar como root." ;     \
